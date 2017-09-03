3eb2b7cb727a14c53a3da8ef7c14081582c92c3d
Knyzhnyk <arina.knizhnik@gmail.com>
2017-09-03 20:09:25 +0300

    Перше домашнє завдання.

---
+++ src/FirstHomework.java
@@ -0,0 +1,6 @@
+public class FirstHomework {
+    public static void main (String[] args){
+        System.out.println("Плани на літо 2018");
+        System.out.println("Буду насолоджуватись життям");
+    }
+}
